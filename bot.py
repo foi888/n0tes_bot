5e4ac5aa7868c2cbc1bb8f9fba81406ec96808ef
foi888 <zblocock@mail.ru>
2021-08-18 17:31:20 +0300

    apdate

--- bot.py
+++ bot.py
@@ -3,20 +3,42 @@ from bot_config import token
 from keyboard import get_main_keyboard
 from database import *
 
+
 bot = telebot.TeleBot(token)
 
+users = {}
 
-@bot.callback_query_handler(func=lambda call: True)
-def reaction_to_button(call):
-    user_id = call.from_user.id
-    if call.data == "show_notes":
+@bot.callback_query_handler(func=lambda button: True)
+def reaction_to_button(button):
+    user_id = button.from_user.id
+    if button.data == "show_notes":
         bot.send_message(user_id, text=get_formatted_notes(user_id))
+    if button.data == "add_note":
+        users.update({user_id: {'title': None, 'description': None}})
+        msg = bot.send_message(user_id, text="введите название")
+        bot.register_next_step_handler(msg, note_title_answer)
+    if button.data == 'delete_note':
+        pass
+
         
+
+def note_title_answer(message):
+    users[message.from_user.id]["title"] = message.text
+    msg = bot.send_message(message.from_user.id, text="Введите описание")
+    bot.register_next_step_handler(msg, note_description_answer)
+
+
+def note_description_answer(message):
+    users[message.from_user.id]["description"] = message.text
+    msg_text = send_note(message.from_user.id, 
+                         users[message.from_user.id]["title"],
+                         users[message.from_user.id]["description"])
+    bot.send_message(message.from_user.id, text=msg_text)
+
 
 @bot.message_handler(content_types=['text'])
 def reaction_to_text(message):
     user_id = message.from_user.id
-    print(user_id)
     if message.text.lower() == 'привет':
         bot.send_message(user_id, "Привет!")
     else:
--- database.py
+++ database.py
@@ -13,10 +13,26 @@ def get_notes(user_id):
 
 def get_formatted_notes(user_id):
     notes = get_notes(user_id)
-    if notes == []:
-        return "У вас нет заметок. Создайте"
-    formatted_text = ""
-    for note in notes:
-        formatted_text += f"Название: {note['Title']}\nОписание: {note['Description']} \n\n"
-    return formatted_text
-    
+    if notes:
+        count = 1
+        formatted_text = ""
+        for note in notes:
+            formatted_text += f"{count}. Название: {note['Title']}\nОписание: {note['Description']} \n\n"
+            count+=1
+        return formatted_text
+    else:
+        return "У вас нет заметок"
+
+
+def send_note(user_id, title, description):
+    url = "https://notes-70b5.restdb.io/rest/notes"
+    payload = json.dumps({'Title': title, 'UserId': user_id, 'Description': description})
+    response = requests.request("POST", url, data=payload, headers=headers)
+    if response.ok:
+        return "Заметка успешно добавлена"
+    else:
+        return "Произошла ошибка. Попробуйте позже"
+
+
+
+        
